bdf5002b2ac7363b8cc0a6426632376a0a494b90
horvatmoharfilip <horvatmoharfilip@gmail.com>
2026-01-20 19:46:47 +0100

    gunsystem

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Target"))
+        {
+            print("hit" + collision.gameObject.name + " !");
+            Destroy(gameObject);
+        }
+
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+            print("hit a wall");
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Camera PlayerCamera;
+    public bool isShooting, readyToShoot;
+    bool allowRest = true;
+    public float shootingDelay = 2f;
+    public int bulletsPerBurst = 3;
+    public int burstBulletsLeft;  
+    public float spreadIntensity;
+
+    public GameObject BulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30;
+    public float BulletPrefabLifeTime = 3f;
+
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstBulletsLeft = bulletsPerBurst;
+    }
+
+    public Vector3 spawnPosition;
+    public Vector3 spawnRotation;
+
+    void Update()
+    {
+        if (currentShootingMode == ShootingMode.Auto)
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+        else if(currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if(readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletsPerBurst;
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+    
+        GameObject bullet = Instantiate(BulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        bullet.transform.forward = shootingDirection;
+
+        bullet.GetComponent<Rigidbody>().AddForce(shootingDirection * bulletVelocity, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBulletAfterTime(bullet, BulletPrefabLifeTime));
+
+        if(allowRest)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowRest = false;
+        }
+
+        if(currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            Invoke("FireWeapon", shootingDelay);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowRest = true;
+    }
+
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if(Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - bulletSpawn.position;
+
+        float x = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        float y = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+
+        return direction + new Vector3(x, y, 0);
+    }
+    
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
