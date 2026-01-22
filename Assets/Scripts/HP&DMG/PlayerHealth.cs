a9a10d985f3c19af343e6c3b4d367a147cee69c9
Ziga <borkoziga44@gmail.com>
2026-01-22 14:43:04 +0100

    hp&dmg_part1

--- Assets/Scripts/HP&DMG/PlayerHealth.cs
+++ Assets/Scripts/HP&DMG/PlayerHealth.cs
@@ -5,14 +5,19 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     private int currentHealth;
 
+    public HealthBar healthBar;
+
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
         if (currentHealth <= 0)
         {
             Die();
