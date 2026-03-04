46315428b8bae9886917280c76e342b2c6fa0ad6
horvatmoharfilip <horvatmoharfilip@gmail.com>
2026-03-04 14:57:54 +0100

    deathscreen

--- Assets/Scripts/HP&DMG/PlayerHealth.cs
+++ Assets/Scripts/HP&DMG/PlayerHealth.cs
@@ -1,4 +1,5 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem; // pomembno za PlayerInput
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -6,29 +7,65 @@ public class PlayerHealth : MonoBehaviour
     private int currentHealth;
 
     public HealthBar healthBar;
+    public GameObject deathScreen;
+    public Transform respawnPoint;
+
+    private CharacterController controller;
+    private PlayerInput playerInput;
 
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(currentHealth);
+
+        deathScreen.SetActive(false);
+
+        controller = GetComponent<CharacterController>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-
         healthBar.SetHealth(currentHealth);
+
         if (currentHealth <= 0)
         {
             Die();
         }
     }
 
-    private void Die()
+    void Die()
     {
-        Debug.Log("Player Died!");
-        // You can add a death animation, game over screen, or respawn logic here.
-        // For now, just destroy the player object.
-        Destroy(gameObject);
+        Time.timeScale = 0f;
+        deathScreen.SetActive(true);
+
+        // Odkleni miško
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        // Izklopi movement/input
+        playerInput.enabled = false;
+    }
+
+    public void Respawn()
+    {
+        Time.timeScale = 1f;
+        deathScreen.SetActive(false);
+
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth);
+
+        // Teleport (pomembno za CharacterController)
+        controller.enabled = false;
+        transform.position = respawnPoint.position;
+        controller.enabled = true;
+
+        // Zakleni miško nazaj
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        // Vklopi input
+        playerInput.enabled = true;
     }
 }
